fa16fb91501230ce66abda8e6293a9206374ef56
sourav0809 <spathak7431@gmail.com>
2025-07-05 17:02:34 +0530

    fix: update server port configuration and improve error handling in index.ts; add .eslintrc.json for linting rules

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,14 +1,15 @@
+/* eslint-disable no-console */
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import morgan from "morgan";
+// import morgan from "morgan";
 import logRoutes from "./routes/logRoutes";
 import { config } from "dotenv";
 
 config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8001;
 
 // Middleware
 app.use(helmet());
@@ -20,20 +21,12 @@ app.use(express.json());
 app.use("/api", logRoutes);
 
 // Error handling
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    console.error(err.stack);
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",
-    });
-  }
-);
+app.use((err: Error, req: express.Request, res: express.Response) => {
+  res.status(500).json({
+    success: false,
+    error: "Internal Server Error",
+  });
+});
 
 // Health check
 app.get("/health", (req, res) => {
