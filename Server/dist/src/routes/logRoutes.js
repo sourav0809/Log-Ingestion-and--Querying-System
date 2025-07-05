7bcd6cc2569ad88877c5c0543765487b4db66c07
sourav0809 <spathak7431@gmail.com>
2025-07-05 18:22:41 +0530

    chore: clean up project by removing unused log-related files and updating tsconfig for improved type safety

--- Server/dist/src/routes/logRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const logController_1 = require("../controllers/logController");
-const router = (0, express_1.Router)();
-router.post("/logs", logController_1.LogController.createLog);
-router.get("/logs", logController_1.LogController.getLogs);
-exports.default = router;
